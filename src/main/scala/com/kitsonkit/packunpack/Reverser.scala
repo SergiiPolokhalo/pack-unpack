c75716a202c93d43a567ea1f6179ce7a3f1e0eac
Serhii Polokhalo <kitsonkit@gmail.com>
2020-06-04 12:15:06 -0400

    Reverser looks fine

--- src/main/scala/com/kitsonkit/packunpack/Reverser.scala
+++ src/main/scala/com/kitsonkit/packunpack/Reverser.scala
@@ -1,8 +1,15 @@
 package com.kitsonkit.packunpack
 
+import scala.annotation.tailrec
+
 /**
  * Object that takes a generic list and can reverse it using a method that allows tail recursion. The test can implement an instance of any concrete object
  */
 object Reverser {
-  def reverse[T](value: List[T]) : List[T] = ???
+  @tailrec
+  def reverse[T](value: List[T], accumulator: List[T] = List.empty[T]): List[T] = value match {
+    case List() => accumulator
+    case List(oneValue) => oneValue :: accumulator
+    case head :: body => reverse(body, head :: accumulator)
+  }
 }
--- src/test/scala/ReverserSuite.scala
+++ src/test/scala/ReverserSuite.scala
@@ -11,6 +11,14 @@ class ReverserSuite extends FunSuite{
   test("Reorder String") {
     assert(Reverser.reverse(List("A","B","CD")) == List("CD","B","A"))
   }
+  test("Reorder mixed types") {
+    assert(Reverser.reverse(List(1,"B",BigDecimal("3.1415"))) == List(BigDecimal("3.1415"),"B",1))
+  }
+
+  test("Double reverse mixed types") {
+    val src = List('A',"B", 0xc, 123L)
+    assert(Reverser.reverse(Reverser.reverse(src)) == src)
+  }
 
 }
 
