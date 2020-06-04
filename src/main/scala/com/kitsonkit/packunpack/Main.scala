c00b73ab12ba92cdee940b75bb8fa15bd9b7dbfe
Serhii Polokhalo <kitsonkit@gmail.com>
2020-06-04 15:09:56 -0400

    Final commit

--- src/main/scala/com/kitsonkit/packunpack/Main.scala
+++ src/main/scala/com/kitsonkit/packunpack/Main.scala
@@ -5,6 +5,13 @@ package com.kitsonkit.packunpack
  */
 object Main {
   def main(args: Array[String]): Unit = {
+    println(" Generic list reverse:")
+    val lst = List("I had a fun", BigDecimal("1000"), "times","playing with this test")
+    println(s"$lst converted to ${Reverser.reverse(lst)}")
+
+    println("Transformation:")
+    List("AAAAABBBCCC","A","AAA","AABBBCCCD").foreach(s => println(s"$s packed to ${ZipUnzip.pack(s)}"))
+    List("5ABC3D","A","3A","2A3B3CD").foreach(s => println(s"$s unpacked to ${ZipUnzip.unpack(s)}"))
   }
 
 }
